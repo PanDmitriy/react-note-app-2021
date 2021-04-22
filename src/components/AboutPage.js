dada9e18cb245d85883772c2dcebf06bf201d2ad
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-04-22 14:29:55 +0300

    changed AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,8 +4,16 @@ export const AboutPage = () => {
 
   return (
     <>
-      <div className="container pt4"> 
-        <h2>About page</h2>
+      <div className="container pt-4"> 
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="container-fluid">
+            <h1 class="display-5 fw-bold">The best React app </h1>
+            <p class="col-md-8 fs-4">
+              Version app <strong>1.0.42</strong> <br/>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero perferendis commodi quasi, dolores dignissimos veritatis assumenda sint, doloribus iusto mollitia, quas voluptas voluptate aperiam!
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
