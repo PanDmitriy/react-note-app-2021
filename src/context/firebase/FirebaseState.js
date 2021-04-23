8ac4b0dae2b0f2220b74010e3520d1d455522ed2
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-04-23 14:32:27 +0300

    changed firebaseReducer

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { FirebaseContext } from "./firebaseContext"
+import { firebaseReducer } from './firebaseReducer';
 
 export const FirebaseState = ({children}) => {
+  const initialState = {
+    notes: [],
+    loading: false,
+  }
+  const [state, dispatch] = useReducer( firebaseReducer(), initialState )
+  
   return (
     <FirebaseContext.Provider>
       {children}
