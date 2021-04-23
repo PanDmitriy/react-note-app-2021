943c2f302d312d712706e87f2f97ea90b387c679
Dmitriy Panzhinskiy <pandimun@gmail.com>
2021-04-23 13:20:26 +0300

    create Firebase Context

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import { Alert } from './components/Alert';
 import { MainPage } from './pages/MainPage';
 import { Navbar } from './components/Navbar';
 import { AlertState } from './context/alert/AlertState';
+import { FirebaseState } from './context/firebase/FirebaseState';
 
 
 function App() {
 
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar/>
-        <Alert />
-        <Switch>
-          <Route component={MainPage} path={'/'} exact/>
-          <Route component={AboutPage} path={'/about'} />
-        </Switch>
-      </BrowserRouter>
-    </AlertState>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar/>
+          <Alert />
+          <Switch>
+            <Route component={MainPage} path={'/'} exact/>
+            <Route component={AboutPage} path={'/about'} />
+          </Switch>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
